ed2540333939b43366d9bf95bd0d225a29c57d7c
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-06-27 13:43:03 +0530

    directory program

---
+++ Problem3.py
@@ -0,0 +1,8 @@
+import os
+#this is the folder path where you want to list the contents
+directory_path = r"C:\Users\kanth\Desktop\Python"
+
+contents = os.listdir(directory_path)
+
+for item in contents:
+    print(item)
