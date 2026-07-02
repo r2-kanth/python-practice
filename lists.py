3566f24c2871d8a728df0409c0961fed2e3f4e02
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-07-02 16:28:01 +0530

    list commands

--- lists.py
+++ lists.py
@@ -3,3 +3,15 @@ print(name)
 print(name[3])
 name[4] = "kanth"
 print(name[4])
+
+
+a = [1,234,45,657,4,3453,56]
+a.sort()
+print(a)
+#a.reverse()
+#a.insert(3,687)
+#a.append(34)
+#a.pop(3)
+print(a.pop(3))
+print(a)
+a.remove(234)
